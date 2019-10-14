21ae1cb67b4407354c7cca3f374f07c1e1910841
andy <ztao8607@gmail.com>
2019-10-14 09:53:02 +0800

    Set grpc invoke timeout to 10s

--- ci-agent/gitlab/implement.go
+++ ci-agent/gitlab/implement.go
@@ -74,7 +74,7 @@ func (s *Service) InvokeBuildService(ntci git.Ntci) (err error) {
 
 	c := build_rpc_v1.NewBuildServiceClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	r, err := c.Run(ctx, &build_rpc_v1.Request{
