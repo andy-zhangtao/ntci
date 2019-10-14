a162c7b18c00ca35ffb5bcd7f153e229ad77c45f
andy <ztao8607@gmail.com>
2019-10-14 14:22:45 +0800

    clear some comments

--- ci-builder/tpl.go
+++ ci-builder/tpl.go
@@ -1,36 +1,7 @@
 package main
 
-//const cloneTpl = `#!/bin/sh
-//cd {{.Root}}; git clone --branch {{.Branch}} "{{.Url}}" {{.Name}} > /build.log 2>&1`
 const cloneTpl = `#!/bin/sh
 cd {{.Root}}; git clone --branch {{.Branch}} "{{.Url}}" {{.Name}} 2>&1`
-
-//const buildTpl = `#!/bin/sh
-//{{range .Env}}
-//echo 'set {{.}}'
-//set {{.}}
-//{{end}}
-//
-//echo "Environment"
-//env >> /build.log
-//echo "  "
-//
-//{{range .BeforeBuild}}
-//echo '{{.}}'
-//{{.}} >> /build.log 2>&1 ||true
-//{{end}}
-//
-//set -e
-//{{range .Build}}
-//echo '{{.}}'
-//{{.}} >> /build.log 2>&1
-//{{end}}
-//
-//{{range .AfterBuild}}
-//echo '{{.}}'
-//{{.}} >> /build.log 2>&1 ||true
-//{{end}}
-//`
 
 const buildTpl = `#!/bin/sh
 echo "------->Environment"
