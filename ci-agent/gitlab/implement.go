3f536f8ba6c2524a40dae7834a80f3aae3cdb276
andy <ztao8607@gmail.com>
2019-10-16 14:13:00 +0800

    add gateway grpc server

--- ci-agent/gitlab/implement.go
+++ ci-agent/gitlab/implement.go
@@ -68,7 +68,8 @@ func (s *Service) InvokeBuildService(ntci git.Ntci) (err error) {
 
 	conn, err := grpc.Dial(bus.Build[bus.BuildMode].Addr, grpc.WithInsecure())
 	if err != nil {
-		logrus.Fatalf("did not connect: %v", err)
+		logrus.Errorf("did not connect: %v", err)
+		return err
 	}
 	defer conn.Close()
 
