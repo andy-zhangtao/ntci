69046b6bb9f4dc4284880739234c4e45621df077
andy <ztao8607@gmail.com>
2019-10-18 09:12:08 +0800

    fix wrong params num issue

--- ci-build/k8s/store/action.go
+++ ci-build/k8s/store/action.go
@@ -117,7 +117,7 @@ Get specify build ID. If there is no build record, create a new one.
 func (p *PGBus) getNextId(b Build) (id int, err error) {
 	query := "SELECT ID FROM id WHERE name=$1 AND owner=$2"
 	logrus.Debugf("GetNextID SQL: %s , name: %s, owner:%s", query, b.Name, b.User)
-	rows, err := p.db.Query(query, b.Name)
+	rows, err := p.db.Query(query, b.Name, b.User)
 	if err != nil {
 		return 0, err
 	}
